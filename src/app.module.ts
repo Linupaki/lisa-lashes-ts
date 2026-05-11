209cd24d6bf90e8d5c183392ee949634c305c6e0
Hlib Korobov <glebkorobov5@gmail.com>
2026-05-11 15:50:58 +0100

    Added static serve

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ServeStaticModule.forRoot({
+      // This is the absolute path to your folder containing index.html
+      rootPath: join(__dirname, '../..', 'front'),
+
+      // Optional: Requests starting with /public will look here
+      // serveRoot: '/public',
+      renderPath: '/',
+    }),
+  ], controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
